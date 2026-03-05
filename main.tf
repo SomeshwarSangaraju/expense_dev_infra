a9735a6c313f22553d231735ba9e00893363ffa6
SSR <someshwar.sangaraju19@gmail.com>
2026-03-05 12:33:11 +0530

    terraform

--- main.tf
+++ main.tf
@@ -4,10 +4,31 @@ resource "aws_instance" "main" {
   instance_type = each.value
   vpc_security_group_ids = [aws_security_group.this.id]
 
-   provisioner "local-exec" {
+  provisioner "local-exec" {
     command = "echo The server's IP address is ${self.private_ip} > inventory.ini"
   }
 
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        password = "DevOps321"
+        host     = self.public_ip
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+        "sudo dnf install nginx -y",
+        "sudo systemctl start nginx"
+        ]
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo systemctl stop nginx",
+        "echo 'successfully stopped nginx server' "
+      ]
+      when = destroy
+    }
   
 
   tags = merge(
