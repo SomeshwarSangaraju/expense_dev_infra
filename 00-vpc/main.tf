7f936d2f64884240000514e71fdbee5a56af4a8b
SSR <someshwar.sangaraju19@gmail.com>
2026-03-08 16:41:55 +0530

    terraform

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -69,27 +69,97 @@ resource "aws_subnet" "database" {
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.vpc_id
+  vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.example.id
-  }
-
-  route {
-    ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
-  }
-
-  tags = {
-    Name = "example"
-  }
+   tags = merge( 
+    local.vpc_tags,
+    {
+        Name = "${local.common_suffix_name}-public"
+    }
+  )
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+   tags = merge( 
+    local.vpc_tags,
+    {
+        Name = "${local.common_suffix_name}-private"
+    }
+  )
+}
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+   tags = merge( 
+    local.vpc_tags,
+    {
+        Name = "${local.common_suffix_name}-database"
+    }
+  )
+}
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.this.id
+}
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+
+}
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+
+}
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.foo.id
-  route_table_id = aws_route_table.bar.id
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public.id[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidrs)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
+}
+
+resource "aws_eip" "elastic_ip" {
+  domain   = "vpc"
+   tags = merge( 
+    local.vpc_tags,
+    {
+        Name = "${local.common_suffix_name}-elastic_ip"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.elastic_ip.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge( 
+    local.vpc_tags,
+    {
+        Name = "${local.common_suffix_name}-nat"
+    }
+  )
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.this.id]
 }
 
 
