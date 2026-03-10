339fa96035026c213b3099bbb321f4b95e1c3b7a
SSR <someshwar.sangaraju19@gmail.com>
2026-03-10 18:40:08 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,33 +4,33 @@ resource "aws_instance" "main" {
   instance_type = each.value
   vpc_security_group_ids = [aws_security_group.this.id]
 
-  provisioner "local-exec" {
-    command = "echo The server's IP address is ${self.private_ip} > inventory"
-  }
+  # provisioner "local-exec" {
+  #   command = "echo The server's IP address is ${self.private_ip} > inventory"
+  # }
 
-    connection {
-        type     = "ssh"
-        user     = "ec2-user"
-        password = "DevOps321"
-        host     = self.public_ip
-    }
+  #   connection {
+  #       type     = "ssh"
+  #       user     = "ec2-user"
+  #       password = "DevOps321"
+  #       host     = self.public_ip
+  #   }
 
-    provisioner "remote-exec" {
-        inline = [
-        "sudo dnf install nginx -y",
-        "sudo systemctl start nginx"
-        ]
-    }
+  #   provisioner "remote-exec" {
+  #       inline = [
+  #       "sudo dnf install nginx -y",
+  #       "sudo systemctl start nginx"
+  #       ]
+  #   }
 
-    provisioner "remote-exec" {
-      inline = [
-        "sudo systemctl stop nginx",
-        "echo 'successfully stopped nginx server' "
-      ]
-      when = destroy
-    }
+  #   provisioner "remote-exec" {
+  #     inline = [
+  #       "sudo systemctl stop nginx",
+  #       "echo 'successfully stopped nginx server' "
+  #     ]
+  #     when = destroy
+  #   }
   
-
+  user_data = "bastion.sh"
   tags = merge(
     local.tags,
     {
@@ -40,24 +40,5 @@ resource "aws_instance" "main" {
   ) 
 }
 
-resource "aws_security_group" "this" {
-
-  name   = "${var.project}-${var.environment}-allow_all"
-
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-}
 
 
