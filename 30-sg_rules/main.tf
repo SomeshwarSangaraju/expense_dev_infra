fb670706cadbed60e4a48a7a2b22860d89787aa1
SSR <someshwar.sangaraju19@gmail.com>
2026-03-15 13:07:26 +0530

    Expense infra

--- 30-sg_rules/main.tf
+++ 30-sg_rules/main.tf
@@ -34,14 +34,14 @@ resource "aws_security_group_rule" "bastion_backend" {
   to_port           = 22
 }
 
-# resource "aws_security_group_rule" "bastion_backend_alb" {
-#   type              = "ingress"
-#   security_group_id = local.backend_sg_id
-#   source_security_group_id = local.bastion_sg_id
-#   from_port         = 22
-#   protocol          = "tcp"
-#   to_port           = 22
-# }
+resource "aws_security_group_rule" "bastion_backend_alb" {
+  type              = "ingress"
+  security_group_id = local.backend_alb_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
 
 resource "aws_security_group_rule" "bastion_frontend" {
   type              = "ingress"
